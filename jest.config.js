88423251423ae58d602b04957e7a42c3f29157cb
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-05-21 17:35:32 -0300

    chore: config jest and eslint

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts{ts, tsx}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
-  trasnform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts{ts, tsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest',
+    },
+}
