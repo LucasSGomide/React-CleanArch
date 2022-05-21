5b568ae9cae7d611da1137684205bcef0d2b56d1
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-05-21 18:38:34 -0300

    feat: ensure RemoteAuth calls HttpPostClient with correct URL

---
+++ src/data/usecases/auth/RemoteAuth.ts
@@ -0,0 +1,12 @@
+import { IHttpPostClient } from '../../protocols/http/IHttpPostClient'
+
+export class RemoteAuth {
+    constructor(
+        private readonly url: string,
+        private readonly httpPostClient: IHttpPostClient
+    ) {}
+
+    async auth(): Promise<void> {
+        return await this.httpPostClient.post(this.url)
+    }
+}
