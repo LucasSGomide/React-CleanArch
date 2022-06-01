812cac9a9c3cbc068a58027d60878a0a283310c1
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-01 20:16:31 -0300

    refactor: rename MockAuth file to MockAccount

--- src/domain/test/MockAuth.ts
+++
@@ -1,7 +0,0 @@
-import faker from 'faker'
-import { AuthParams } from '@/domain/usecases/IAuthentication'
-
-export const createMockAuthParams = (): AuthParams => ({
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-})
