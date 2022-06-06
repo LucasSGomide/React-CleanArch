821864ae0a9ab4318a6da0466d03e980edd9ae20
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-06 20:53:38 -0300

    test: ensure AxiosHttpClient calls axios with correct HTTP verb

--- src/infra/http/axios/client/AxiosHttpClient.spec.ts
+++ src/infra/http/axios/client/AxiosHttpClient.spec.ts
@@ -13,12 +13,12 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-    test('Should call axios with correct URL', async () => {
+    test('Should call axios with correct URL and HTTP verb', async () => {
         const url = faker.internet.url()
         const sut = makeSut()
 
         await sut.post({ url })
 
-        expect(mockedAxios).toHaveBeenCalledWith(url)
+        expect(mockedAxios.post).toHaveBeenCalledWith(url)
     })
 })
