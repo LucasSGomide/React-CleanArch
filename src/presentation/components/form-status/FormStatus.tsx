ff33a4ea5c90190fe76eaab738210075b78dbfd0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-09 22:15:45 -0300

    feat: ensure login will not render spinner and error on mount

--- src/presentation/components/form-status/FormStatus.tsx
+++ src/presentation/components/form-status/FormStatus.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status-styles.scss'
 
 import Spinner from '../spinner/Spinner'
+import FormContext from '@/presentation/context/form/FormContext'
 
 const FormStatus: React.FC = () => {
+    const { isLoading, errorMessage } = useContext(FormContext)
+
     return (
-        <div className={Styles.errorContainer}>
-            <Spinner className={Styles.spinner} />
-            <span className={Styles.error}>Erro</span>
+        <div data-testid="error-container" className={Styles.errorContainer}>
+            {isLoading && <Spinner className={Styles.spinner} />}
+            {errorMessage && (
+                <span className={Styles.error}>{errorMessage}</span>
+            )}
         </div>
     )
 }
--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 
 import {
@@ -7,33 +7,44 @@ import {
     FormStatus,
     Footer,
 } from '@/presentation/components'
+import FormContext from '@/presentation/context/form/FormContext'
+
+type LoginState = {
+    isLoading: boolean
+    errorMessage: string
+}
 
 const Login: React.FC = () => {
+    const [state] = useState<LoginState>({ isLoading: false, errorMessage: '' })
+
     return (
         <div className={Styles.login}>
             <LoginHeader />
-            <form className={Styles.form}>
-                <h2>Login</h2>
 
-                <Input
-                    type="email"
-                    name="email"
-                    placeholder="Your e-mail here..."
-                />
+            <FormContext.Provider value={state}>
+                <form className={Styles.form}>
+                    <h2>Login</h2>
 
-                <Input
-                    type="password"
-                    name="password"
-                    placeholder="Your password here..."
-                />
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Your e-mail here..."
+                    />
 
-                <button className={Styles.submit} type="submit">
-                    Sign In
-                </button>
-                <span className={Styles.link}>Sign Up</span>
+                    <Input
+                        type="password"
+                        name="password"
+                        placeholder="Your password here..."
+                    />
 
-                <FormStatus />
-            </form>
+                    <button className={Styles.submit} type="submit">
+                        Sign In
+                    </button>
+                    <span className={Styles.link}>Sign Up</span>
+
+                    <FormStatus />
+                </form>
+            </FormContext.Provider>
             <Footer />
         </div>
     )
