898b0e3143bf2cb7c6d4a7c30ab860b1e6b434b1
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-13 22:05:46 -0300

    test: ensure login calls authentication with correct values

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import userEvent from '@testing-library/user-event'
 import faker from 'faker'
-
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
 import { UserEvent } from '@testing-library/user-event/dist/types/setup'
 import '@testing-library/jest-dom/extend-expect'
 
 import Login from './Login'
 import { ValidationSpy } from '@/presentation/test'
+import { AccountModel } from '@/domain/models'
+import { AuthParams, IAuthentication } from '@/domain/usecases'
+import { createMockAccountModel } from '@/domain/test'
+
+class AuthenticationSpy implements IAuthentication {
+    account = createMockAccountModel()
+    params: AuthParams
+
+    async auth(params: AuthParams): Promise<AccountModel> {
+        this.params = params
+
+        return this.account
+    }
+}
 
 type SutTypes = {
     validationSpy: ValidationSpy
     user: UserEvent
+    authenticationSpy: AuthenticationSpy
 }
 
 type SutParams = {
@@ -20,14 +34,18 @@ type SutParams = {
 
 const makeSut = (params?: SutParams): SutTypes => {
     const validationSpy = new ValidationSpy()
+    const authenticationSpy = new AuthenticationSpy()
 
     validationSpy.errorMessage = params?.validationError
 
     const user = userEvent.setup()
-    render(<Login validation={validationSpy} />)
+    render(
+        <Login validation={validationSpy} authentication={authenticationSpy} />
+    )
 
     return {
         validationSpy,
+        authenticationSpy,
         user,
     }
 }
@@ -171,5 +189,26 @@ describe('Login', () => {
         const spinner = screen.getByTestId('spinner')
 
         expect(spinner).toBeInTheDocument()
+    })
+
+    test('Should call authentication with correct values', async () => {
+        const { user, authenticationSpy } = makeSut()
+
+        const email = faker.internet.email()
+        const password = faker.internet.password()
+
+        const emailInput = screen.getByRole('textbox')
+        const passwordInput = screen.getByPlaceholderText(
+            'Your password here...'
+        )
+
+        await user.type(emailInput, email)
+        await user.type(passwordInput, password)
+
+        const signInButton = screen.getByRole('button', { name: 'Sign In' })
+
+        await userEvent.click(signInButton)
+
+        expect(authenticationSpy.params).toEqual({ email, password })
     })
 })
