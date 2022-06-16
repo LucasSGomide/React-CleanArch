ab1271d57f7ac707425511bb70bbf2a113f15e1d
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-15 22:53:58 -0300

    test: ensure MinLengthValidation return falsy if length is valid

--- src/validation/validators/min-length/MinLengthValidation.spec.ts
+++ src/validation/validators/min-length/MinLengthValidation.spec.ts
@@ -7,8 +7,14 @@ describe('MinLengthValidation', () => {
 
         const error = sut.validate('')
 
-        console.log(error.message)
-
         expect(error).toBeInstanceOf(InvalidFieldLengthError)
+    })
+
+    test('Should return falsu if value length is valid', () => {
+        const sut = new MinLengthValidation('Field', 5)
+
+        const error = sut.validate('VALID_WORD')
+
+        expect(error).toBeFalsy()
     })
 })
