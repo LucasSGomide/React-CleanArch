020a0914bdc01a255d325b43cdae16636cdc1c93
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-18 14:38:24 -0300

    refator: adjust Login validation factory

--- src/main/factories/pages/login/LoginValidationFactory.ts
+++ src/main/factories/pages/login/LoginValidationFactory.ts
@@ -3,8 +3,8 @@ import { ValidationBuilder } from '@/validation/validators/validation-builder/Va
 
 export const makeLoginValidation = (): ValidationComposite => {
     const validationComposite = ValidationComposite.build([
-        ...ValidationBuilder.field('password').minLength(5).required().build(),
         ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().minLength(5).build(),
     ])
 
     return validationComposite
