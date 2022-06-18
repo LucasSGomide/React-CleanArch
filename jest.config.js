340f82f4d6160698680ba578c2dae73bd707b3d5
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-18 15:17:00 -0300

    chore: add files do test coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     roots: ['<rootDir>/src'],
     collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.{ts, tsx}', '!**/*.d.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}',
+        '!**/*.d.ts',
+        '!<rootDir>/src/main/**/*',
+        '!<rootDir>/src/presentation/components/router/**/*',
+    ],
     coveragePathIgnorePatterns: ['index.ts'],
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
