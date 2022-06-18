396c47105d73c3b4a8eaf0dedd67bca1ef6e7074
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-17 21:41:49 -0300

    feat: ensure ValidationBuilder returns a RequiredFieldValidation

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/EmailValidation'
+export * from './min-length/MinLengthValidation'
+export * from './required-field/RequiredFieldValidation'
+export * from './validation-composite/ValidationComposite'
---
+++ src/validation/validators/validation-builder/ValidationBuilder.ts
@@ -0,0 +1,28 @@
+import { IFieldValidation } from '@/validation/protocols/FieldValidation'
+import { RequiredFieldValidation } from '@/validation/validators'
+
+export class ValidationBuilder {
+    private constructor(
+        // fieldName string receives a value when the static method field is accessed
+        private readonly fieldName: string,
+        // validations array store a new validation method for each loop
+        private readonly validations: IFieldValidation[]
+    ) {}
+
+    // entry point - instanciates a ValidationBuilder
+    static field(fieldName: string): ValidationBuilder {
+        return new ValidationBuilder(fieldName, [])
+    }
+
+    // method to generate a RequiredFieldValidation e return an ValidationBuilder updated instance
+    required(): ValidationBuilder {
+        this.validations.push(new RequiredFieldValidation(this.fieldName))
+
+        return this
+    }
+
+    // returns an array of all builded validations
+    build(): IFieldValidation[] {
+        return this.validations
+    }
+}
