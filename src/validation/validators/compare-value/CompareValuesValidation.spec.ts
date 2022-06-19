1746b9688247d9507f5a4a10e5eef7d4fee2f453
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-19 16:42:53 -0300

    test: ensure CompareValuesValidation returns falsy if comparison is valid

--- src/validation/validators/compare-value/CompareValuesValidation.spec.ts
+++ src/validation/validators/compare-value/CompareValuesValidation.spec.ts
@@ -7,11 +7,21 @@ const makeSut = (valueToCompare: string): CompareValuesValidation =>
     new CompareValuesValidation(faker.database.column(), valueToCompare)
 
 describe('CompareValuesValidation', () => {
-    test('Should return InvalidMatchError if value is invalid', () => {
+    test('Should return InvalidMatchError if comparison is invalid', () => {
         const sut = makeSut(faker.random.word())
 
         const error = sut.validate(faker.random.word())
 
         expect(error).toBeInstanceOf(InvalidMatchError)
+    })
+
+    test('Should return falsy if comparison is valid', () => {
+        const validValue = faker.random.word()
+
+        const sut = makeSut(validValue)
+
+        const error = sut.validate(validValue)
+
+        expect(error).toBeFalsy()
     })
 })
