59970728e716d4b524609242dcf4eb053d32a105
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-18 22:07:08 -0300

    feat: ensure LocalSaveAccesToken calls SetStorage with correct value

---
+++ src/data/test/MockStorage.ts
@@ -0,0 +1,12 @@
+import { ISetStorage } from '@/data/protocols/cache/SetStorage'
+
+export class SetStorageSpy implements ISetStorage {
+    key: string
+    value: any
+
+    async set(key: string, value: string): Promise<void> {
+        this.key = key
+        this.value = value
+        // localStorage.setItem(key, value)
+    }
+}
